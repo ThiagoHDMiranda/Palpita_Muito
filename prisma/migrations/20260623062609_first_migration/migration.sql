a4b1491f0f54c85312c9c590a59dc3090ee13fe6
ThiagoHDMiranda <thiagohdmiranda7@gmail.com>
2026-06-23 03:27:33 -0300

    reset DB and recreate migration

--- prisma/migrations/20260623062609_first_migration/migration.sql
+++ prisma/migrations/20260623062609_first_migration/migration.sql
@@ -1,8 +1,12 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
