20bf743f64b142fe62fff38b4484835187a8be34
ThiagoHDMiranda <thiagohdmiranda7@gmail.com>
2026-07-19 22:23:35 -0300

    remove comments

--- components/cardMatch.tsx
+++ components/cardMatch.tsx
@@ -174,26 +174,13 @@ function Result({ result, currentGuess }: ResultProps) {
         </span>
       )}
       <div className="col-start-2 flex gap-2 justify-center text-(--secondary) bg-gray-300 font-bold rounded-2xl relative">
-        <span
-          className={`w-6 h-6 text-center relative 
-            `}
-          // ${result && result.homePenalties && `before:absolute before:right-0 before:top-0 before:text-[10px] before:content-[${result.homePenalties}]`}
-        >
+        <span className="w-6 h-6 text-center relative">
           {result ? result.homeGoals : "-"}
         </span>
         <span>x</span>
-        <span
-          className={`w-6 h-6 text-center relative 
-            `}
-          // ${result && result.awayPenalties && `before:absolute before:right-0 before:top-0 before:text-[10px] before:content-[${result.awayPenalties}]`}
-        >
+        <span className="w-6 h-6 text-center relative">
           {result ? result.awayGoals : "-"}
         </span>
-        {/* {result?.extraTime && (
-          <span className="absolute w-10 right-0 text-center self-center text-sm">
-            P
-          </span>
-        )} */}
       </div>
       <div className="col-start-3 text-center self-center text-(--primary-60) text-sm font-bold">
         {result ? "Finalizado" : ""}
